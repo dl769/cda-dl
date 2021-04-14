c4644f5556e9fd611e21b73ed17876a8d0481291
dl769 <49956347+dl769@users.noreply.github.com>
2021-04-14 10:29:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,81 +39,71 @@ def index():
     
 def getUrl(url):
     if url.find('cda.pl/') != -1: 
-        try:
-            gChromeOptions = webdriver.ChromeOptions()
-            gChromeOptions.add_argument("window-size=1920x1480")
-            gChromeOptions.add_argument("disable-dev-shm-usage")
-            gDriver = webdriver.Chrome(
-                chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-            )
-            gDriver.get(url)
-            #print(gDriver.page_source)
-            gDriver.close()
-            
-            #GET_URL
-            pSrc = gDriver.page_source
+        gChromeOptions = webdriver.ChromeOptions()
+        gChromeOptions.add_argument("window-size=1920x1480")
+        gChromeOptions.add_argument("disable-dev-shm-usage")
+        gDriver = webdriver.Chrome(
+            chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
+        )
+        gDriver.get(url)
+        #print(gDriver.page_source)
+        gDriver.close()
         
-            x = pSrc.find('.mp4')
-            a = x - 125                         #~~url
-            b = x + 4                           #.mp4 => +4
-            print(x)                            #.mp4 on charNo
-        
-            cutSrc = pSrc[a:b]                  #cut
-            print(cutSrc)
-        
-            y = cutSrc.find('https://')         #obtaining charNo of https...mp4
-            url = cutSrc[y:]        
-        
-            print(url)
-            #GET_TITLE
-            titleStart = pSrc.find('<h1>')
-            titleEnd = pSrc.find('</h1>')
-            titleStart=titleStart+4
-            title = pSrc[titleStart:titleEnd]
-            data = [0 for i in range(2)] 
-            data[0] = url
-            data[1] = title
-            print(data)
-            print(type(data))
-            return data
-        
-        except WebDriverException:
-            print("failed to start driver at path:yyy")
-            return 'error'
+        #GET_URL
+        pSrc = gDriver.page_source
     
+        x = pSrc.find('.mp4')
+        a = x - 125                         #~~url
+        b = x + 4                           #.mp4 => +4
+        print(x)                            #.mp4 on charNo
+    
+        cutSrc = pSrc[a:b]                  #cut
+        print(cutSrc)
+    
+        y = cutSrc.find('https://')         #obtaining charNo of https...mp4
+        url = cutSrc[y:]        
+    
+        print(url)
+        #GET_TITLE
+        titleStart = pSrc.find('<h1>')
+        titleEnd = pSrc.find('</h1>')
+        titleStart=titleStart+4
+        title = pSrc[titleStart:titleEnd]
+        data = [0 for i in range(2)] 
+        data[0] = url
+        data[1] = title
+        print(data)
+        print(type(data))
+        return data
+
     else:
         return 'error'
 def getBestQuality(url):
     if url.find('cda.pl/') != -1: 
-        try:
-            gChromeOptions = webdriver.ChromeOptions()
-            gChromeOptions.add_argument("window-size=1920x1480")
-            gChromeOptions.add_argument("disable-dev-shm-usage")
-            gDriver = webdriver.Chrome(
-                chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-            )
-            gDriver.get(url)
-            #print(gDriver.page_source)
-            gDriver.close()
-        
-            pSrc = gDriver.page_source
-        
-            ssP = pSrc.find('class="quality-txt"')
-            ssK = ssP + 650                                               
-            print(ssP)
-            cutSrc = pSrc[ssP:ssK]    
+        gChromeOptions = webdriver.ChromeOptions()
+        gChromeOptions.add_argument("window-size=1920x1480")
+        gChromeOptions.add_argument("disable-dev-shm-usage")
+        gDriver = webdriver.Chrome(
+            chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
+        )
+        gDriver.get(url)
+        #print(gDriver.page_source)
+        gDriver.close()
     
-            print(cutSrc)
-            if cutSrc.find('1080') != -1:
-                return '1080p'
-            if cutSrc.find('720') != -1:
-                return '720p'
-            return '480p'
-        
-        except WebDriverException:
-            print("failed to start driver at path: xxx")
-            return 'error'
+        pSrc = gDriver.page_source
     
+        ssP = pSrc.find('class="quality-txt"')
+        ssK = ssP + 650                                               
+        print(ssP)
+        cutSrc = pSrc[ssP:ssK]    
+
+        print(cutSrc)
+        if cutSrc.find('1080') != -1:
+            return '1080p'
+        if cutSrc.find('720') != -1:
+            return '720p'
+        return '480p'
+
     else:
         return 'error'
 
